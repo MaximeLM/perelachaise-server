9699009002db5580ad0792266d3625edd25f9fe5
Maxime Le Moine <lm.maxime@gmail.com>
2014-03-04 23:08:36 +0100

    Ajout des modèles NodeOSM et DetailTombe et des classes d'administration associées

--- perelachaise/admin.py
+++ perelachaise/admin.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 
-from perelachaise.models import Tombe
+from perelachaise.models import Tombe, NodeOSM, DetailTombe
 
 class TombeAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -27,5 +27,116 @@ class TombeAdmin(admin.ModelAdmin):
         return '<a href="%s">%s</a>' % (obj.url_wikipedia, obj.url_wikipedia)
     lien_wikipedia.allow_tags = True
 
-# Déclaration des objets pour l'interface administrateur
+class NodeOSMAdmin(admin.ModelAdmin):
+    """
+    Classe d'administration de NodeOSM
+    """
+    
+    # ================
+    # Liste des objets
+    # ================
+    
+    # Liste des champs affichés
+    list_display = ('nom','id',
+                    'latitude','longitude',
+                    'date_creation','date_modification')
+    
+    # Ordre par défaut des champs
+    ordering = ('nom',)
+    
+    # Liste des champs recherchables
+    search_fields = ('nom',)
+    
+    # Actions administrateur disponibles
+    actions=None
+    
+    # =================
+    # Détail d'un objet
+    # =================
+    
+    # Liste des champs affichés
+    fieldsets = [
+        (None, {'fields': ['id','nom']}),
+        (u'Position', {'fields': ['latitude','longitude']}),
+        (u'Suivi', {'fields': ['date_creation','date_modification']}),
+    ]
+    
+    # Champs en lecture seule
+    readonly_fields = ('date_creation','date_modification',)
+
+class DetailTombeAdmin(admin.ModelAdmin):
+    """
+    Classe d'administration de DetailTombe
+    """
+    
+    # ================
+    # Liste des objets
+    # ================
+    
+    # Liste des champs affichés
+    list_display = ('nom_complet','controle',
+                    'lien_wikidata','lien_wikipedia',
+                    'date_creation','date_modification')
+    
+    # Ordre par défaut des champs
+    ordering = ('nom',)
+    
+    # Liste des champs recherchables
+    search_fields = ('nom',)
+    
+    # Actions administrateur disponibles
+    actions=None
+    
+    # =================
+    # Détail d'un objet
+    # =================
+    
+    # Liste des champs affichés
+    fieldsets = [
+        (None, {u'fields': ['node_osm','lien_node_osm']}),
+        (u'Détail', {'fields': ['prenom',
+                                'nom',
+                                'code_wikidata',
+                                'lien_wikidata',
+                                'url_wikipedia',
+                                'date_naissance',
+                                'date_deces',
+                                'activite',
+                                'resume',
+                                ]}),
+        (u'Suivi', {'fields': ['date_creation','date_modification']}),
+        (None, {u'fields': ['controle']}),
+    ]
+    
+    # Champs en lecture seule
+    readonly_fields = ('date_creation','date_modification',
+                       'lien_node_osm','lien_wikidata',)
+    
+    # ====================
+    # Méthodes d'affichage
+    # ====================
+    
+    def lien_wikidata(self, obj):
+        ''' Affiche un lien vers la page wikidata '''
+        return '<a href="http://www.wikidata.org/wiki/%s?uselang=fr">%s</a>' % (obj.code_wikidata, obj.code_wikidata)
+    lien_wikidata.allow_tags = True
+    
+    def lien_wikipedia(self, obj):
+        ''' Affiche un lien vers la page wikipedia '''
+        return '<a href="%s">%s</a>' % (obj.url_wikipedia, obj.url_wikipedia)
+    lien_wikipedia.allow_tags = True
+    
+    def lien_node_osm(self, obj):
+        ''' Affiche un lien vers la page d'administration
+        du node OSM associé '''
+        return '<a href="../../nodeosm/%d">%s</a>' % (obj.node_osm.id,obj.node_osm.nom)
+    lien_node_osm.allow_tags = True
+    
+    def nom_complet(self, obj):
+        ''' Affiche le nom complet de l'objet '''
+        return unicode(obj)
+
+# Déclaration des classes pour l'interface administrateur
 admin.site.register(Tombe, TombeAdmin)
+admin.site.register(NodeOSM, NodeOSMAdmin)
+admin.site.register(DetailTombe, DetailTombeAdmin)
--- perelachaise/models.py
+++ perelachaise/models.py
@@ -19,4 +19,97 @@ class Tombe(models.Model):
     url_wikipedia = models.URLField(blank=True)
     
     def __unicode__(self):
-            return self.nom_osm
+        return self.nom_osm
+
+class NodeOSM(models.Model):
+    """
+    Représente un noeud OSM correspondant à une tombe.
+    """
+    
+    # Identifiant unique OSM <id> utilisé comme clé primaire
+    # Surchargé pour ne pas générer de séquence
+    id = models.IntegerField(primary_key=True)
+    
+    # Nom du node OSM <name>
+    nom = models.CharField(max_length=255)
+    
+    # Latitude du point <lat>
+    latitude = models.DecimalField(max_digits=10, decimal_places=7)
+    
+    # Latitude du point <lon>
+    longitude = models.DecimalField(max_digits=10, decimal_places=7)
+    
+    # ==========
+    # Timestamps
+    # ==========
+    
+    # Date de création
+    # Mis à jour à la création de l'objet
+    date_creation = models.DateTimeField(auto_now_add=True)
+    
+    # Date de modification
+    # Mis à jour à la sauvegarde de l'objet
+    date_modification = models.DateTimeField(auto_now=True)
+    
+    def __unicode__(self):
+        return self.nom
+
+class DetailTombe(models.Model):
+    """
+    Définit une personne ou un monument relatif à un noeud OSM.
+    """
+    
+    # Noeud OSM auquel se rattache le détail
+    node_osm = models.ForeignKey('NodeOSM')
+    
+    # Flag indiquant si l'objet a été validé pour publication
+    controle = models.BooleanField(default=False)
+    
+    # =================
+    # Champs optionnels
+    # =================
+    
+    # Code wikidata, par exemple 'Q123456'
+    code_wikidata = models.CharField(max_length=20, blank=True)
+    
+    # URL de la page wikipedia
+    url_wikipedia = models.URLField(blank=True)
+    
+    # Prénom
+    prenom = models.CharField(max_length=255, blank=True)
+    
+    # Nom
+    nom = models.CharField(max_length=255)
+    
+    # Date de naissance
+    date_naissance = models.DateField(blank=True, null=True)
+    
+    # Date de décès
+    date_deces = models.DateField(blank=True, null=True)
+    
+    # Activité
+    activite = models.CharField(max_length=255, blank=True)
+    
+    # Résumé
+    # Correspond en général à l'introduction de la page wikipedia
+    resume = models.TextField(blank=True)
+    
+    # ==========
+    # Timestamps
+    # ==========
+    
+    # Date de création
+    # Mis à jour à la création de l'objet
+    date_creation = models.DateTimeField(auto_now_add=True)
+    
+    # Date de modification
+    # Mis à jour à la sauvegarde de l'objet
+    date_modification = models.DateTimeField(auto_now=True)
+    
+    def __unicode__(self):
+        if not self.prenom:
+            # Pas de prénom : affichage du nom seul
+            return self.nom
+        else:
+            # Prénom présent : affichage concaténé
+            return self.prenom + ' ' + self.nom
