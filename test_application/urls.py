2f4b1272e13e97e5dbb00d4baef432ce83c06603
Maxime Le Moine <lm.maxime@gmail.com>
2014-03-19 23:12:28 +0100

    Ajout des fixtures Monuments

--- test_application/urls.py
+++ test_application/urls.py
@@ -7,4 +7,5 @@ from test_application import views
 urlpatterns = patterns('',
     url(r'^monumentall/nodeOSM/(?P<name>\w+)$', views.monumentall_nodeOSM, name='monumentall_nodeOSM'),
     url(r'^monumentall/personnalite/(?P<name>\w+)$', views.monumentall_personnalite, name='monumentall_personnalite'),
+    url(r'^monumentall/monument/(?P<name>\w+)$', views.monumentall_monument, name='monumentall_monument'),
 )
--- test_application/views.py
+++ test_application/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.http import require_http_methods
 
 from perelachaise.models import Monument, Personnalite, NodeOSM
-from webservice.views import prepare_json_nodeOSM_for_monument_all, dump_json, prepare_json_personnalite_for_monument_all
+from webservice.views import dump_json, prepare_json_nodeOSM_for_monument_all, prepare_json_personnalite_for_monument_all, prepare_json_monument_for_monument_all
 
 @require_http_methods(["GET"])
 def monumentall_nodeOSM(request, name):
@@ -44,7 +44,7 @@ def monumentall_personnalite(request, name):
     """
     Renvoie la fixture de personnalité pour la vue monument/all/ correspondant au nom indiqué
     """
-    print name
+    
     if name == 'personnalite1':
         # Jim Morrison
         personnalite = Personnalite.objects.get(pk=120)
@@ -70,5 +70,81 @@ def monumentall_personnalite(request, name):
         
     # Construction du résultat
     result = prepare_json_personnalite_for_monument_all(personnalite)
+    
+    return HttpResponse(dump_json(result), mimetype='application/json; charset=utf-8')
+
+@require_http_methods(["GET"])
+def monumentall_monument(request, name):
+    """
+    Renvoie la fixture de monument pour la vue monument/all/ correspondant au nom indiqué
+    """
+    
+    # Chargement des objets
+    
+    monument1 = Monument.objects.get(pk=164)            # Mur des Fédérés (full + UTF8)
+    monument2 = Monument.objects.get(pk=120)            # Jim Morrison (vide)
+    
+    personnalite1 = Personnalite.objects.get(pk=207)    # Alain Bashung
+    personnalite2 = Personnalite.objects.get(pk=76)     # Georges Courteline
+    personnalite3 = Personnalite.objects.get(pk=120)    # Jim Morrison
+    personnalite4 = Personnalite.objects.get(pk=231)    # François d'Astier de La Vigerie
+    
+    if name == 'monument1':
+        monument = monument1
+        
+        # Construction du résultat
+        result = prepare_json_monument_for_monument_all(monument)
+        
+        # Ajout d'une personnalité
+        result_personnalite1 = prepare_json_personnalite_for_monument_all(personnalite1)
+        result['personnalites'] = [result_personnalite1]
+        
+    elif name == 'monument1_update':
+        monument = monument1
+        
+        # Update node OSM
+        monument.node_osm.latitude = Decimal('42.4')
+        monument.node_osm.longitude = Decimal('-42.4')
+        
+        # Construction du résultat
+        result = prepare_json_monument_for_monument_all(monument)
+        
+        # Ajout d'une autre personnalité
+        result_personnalite2 = prepare_json_personnalite_for_monument_all(personnalite2)
+        result['personnalites'] = [result_personnalite2]
+        
+    elif name == 'monument2':
+        monument = monument2
+        
+        # Construction du résultat
+        result = prepare_json_monument_for_monument_all(monument)
+        
+        # Suppression des personnalités
+        result['personnalites'] = []
+        
+    elif name == 'monument2_update1':
+        monument = monument2
+        
+        # Construction du résultat
+        result = prepare_json_monument_for_monument_all(monument)
+        
+    elif name == 'monument2_update2':
+        monument = monument2
+        
+        # Construction du résultat
+        result = prepare_json_monument_for_monument_all(monument)
+        
+        # Modification de la personnalité présente
+        result_personnalite3 = prepare_json_personnalite_for_monument_all(personnalite3)
+        result_personnalite3['nom'] = u'Nouveau nom'
+        result_personnalite3['resume'] = u'Nouveau résumé'
+        
+        # Ajout d'une personnalité
+        result_personnalite4 = prepare_json_personnalite_for_monument_all(personnalite4)
+        
+        result['personnalites'] = [result_personnalite3, result_personnalite4]
+    
+    else:
+        return HttpResponseBadRequest()
     
     return HttpResponse(dump_json(result), mimetype='application/json; charset=utf-8')
